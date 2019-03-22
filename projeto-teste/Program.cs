c20e63519435a0bf54e550017650802570dd8463
mykson wyrlley <mykson.banner@gmail.com>
2019-03-22 12:33:33 -0300

    Acrescentada a pertuda, como vce estar

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -9,6 +9,7 @@ namespace projeto_teste
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom Dia");
             Console.WriteLine("Mykson Bnadeira");
+            Console.WriteLine("Como voce estar?");
 
             Console.ReadLine();
         }
